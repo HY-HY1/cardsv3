01b7adf2661db2b4e5d4da1fa987c5447eeab50b
Harvey Yemm <hjyemm@icloud.com>
2025-07-16 17:17:22 +0100

    refactor (iteration-2) Moved Error Handling to centralised Logic

--- client/app/api/user/route.ts
+++ client/app/api/user/route.ts
@@ -3,6 +3,8 @@ import { User } from "@/lib/models/User";
 import { verifyToken } from "@/utils/jwt/auth";
 import { error } from "console";
 import { getUserByEmail } from "@/utils/cache/userCache";
+import { ServerErrorHandler } from "@/utils/errors/ServerError";
+import { MongoConnect } from "@/lib/mongo/MongoConnect";
 
 export async function GET(res:  Response) {
     try {
@@ -11,6 +13,8 @@ export async function GET(res:  Response) {
         if(!token) {
             return NextResponse.json({ error: "No token Provided"}, {status: 400 })
         }
+
+        await MongoConnect()
 
         const verifiedToken = verifyToken(token)
 
@@ -24,9 +28,17 @@ export async function GET(res:  Response) {
 
 
     } catch (err: unknown) {
-        return NextResponse.json(
-            { error: "There was an error"},
-            { status: 500}
-        )
+        console.log(err)
+
+        return ServerErrorHandler(err)
+    }
+}
+
+export async function DELETE(res: Response) {
+    try {
+
+    } catch (err:unknown) {
+        console.log(err)
+        return ServerErrorHandler(err)
     }
 }
