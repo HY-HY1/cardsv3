778c66cac3d594c389115bbbf1b07a611d1db0b0
Harvey Yemm <hjyemm@icloud.com>
2025-07-16 17:17:48 +0100

    refactor (iteration-2) Updated Server Error Logic

--- client/utils/errors/ServerError.ts
+++ client/utils/errors/ServerError.ts
@@ -1,10 +1,17 @@
 import { NextResponse  } from "next/server";
+import { AppError } from "./Errors";
 
-export function ServerErrorHandler(err:unknown) {
-    if (err instanceof Error) {
-        return NextResponse.json({ error: "There was an internal server error"}, { status: 500})
-    }
-    else {
-        return NextResponse.json({},{status: 500})
-    }
+
+
+
+export function ServerErrorHandler(err: unknown) {
+  if (err instanceof AppError) {
+    return NextResponse.json({ error: err.message }, { status: err.statusCode });
+  }
+
+  if (err instanceof Error) {
+    return NextResponse.json({ error: "There was an internal server error" }, { status: 500 });
+  }
+
+  return NextResponse.json({}, { status: 500 });
 }
